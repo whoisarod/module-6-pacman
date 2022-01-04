941ffb4dfa12040d3f18a5d80eae6f03501995db
Angel Rodriguez <angelrodri@hotmail.com>
2022-01-04 15:10:50 -0800

    updated js file and css file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const width = 28;
 const grid = document.querySelector(".grid");
-const score = document.getElementById("score");
+const scoreDisplay = document.getElementById("score");
 let squares = [];
+let score = 0;
 
 // 28*28 = 784
     // 0 - pac-dots
@@ -56,6 +57,8 @@ function createBoard() {
             squares[i].classList.add("pac-dot");
         } else if (layout[i] === 1) {
             squares[i].classList.add("wall");
+        } else if (layout[i] === 2) {
+            squares[i].classList.add("ghost-lair");
         } else if (layout[i] === 3) {
             squares[i].classList.add("power-pellet");
         }
@@ -76,6 +79,7 @@ function control(e) {
         case 40:
             console.log("pressed down");
             if (
+                !squares[pacmanCurrentIndex + width].classList.contains("ghost-lair") &&
                 !squares[pacmanCurrentIndex + width].classList.contains("wall") &&
                 pacmanCurrentIndex + width < width * width
                 ) 
@@ -84,14 +88,19 @@ function control(e) {
         case 39:
             console.log("pressed right");
             if (
+                !squares[pacmanCurrentIndex + 1].classList.contains("ghost-lair") &&
                 !squares[pacmanCurrentIndex + 1].classList.contains("wall") &&
                 pacmanCurrentIndex % width < width - 1
                 ) 
                 pacmanCurrentIndex +=1;
+                if (pacmanCurrentIndex === 391) {
+                    pacmanCurrentIndex = 364;
+                }
         break;
         case 38:
             console.log("pressed up");
             if (
+                !squares[pacmanCurrentIndex - width].classList.contains("ghost-lair") &&
                 !squares[pacmanCurrentIndex - width].classList.contains("wall") &&
                 pacmanCurrentIndex - width >= 0
                 ) 
@@ -100,12 +109,77 @@ function control(e) {
         case 37:
             console.log("pressed left");
             if (
+                !squares[pacmanCurrentIndex - 1].classList.contains("ghost-lair") &&
                 !squares[pacmanCurrentIndex - 1].classList.contains("wall") &&
                 pacmanCurrentIndex % width !== 0
                 ) 
                 pacmanCurrentIndex -=1;
+                if (pacmanCurrentIndex === 364) {
+                    pacmanCurrentIndex = 391;
+                }
         break;
     }
     squares[pacmanCurrentIndex].classList.add("pacman");
+    pacDotEaten();
 }
 document.addEventListener("keyup", control);
+
+function pacDotEaten() {
+    if (squares[pacmanCurrentIndex].classList.contains("pac-dot")) {
+        squares[pacmanCurrentIndex].classList.remove("pac-dot");
+        score++;
+        scoreDisplay.innerHTML = score;
+    }
+}
+
+class Ghost {
+    constructor(className, startIndex, speed) {
+        this.className = className;
+        this.startIndex = startIndex;
+        this.speed = speed;
+        this. currentIndex = startIndex;
+        this.isScared = false;
+        this.timerId = NaN;
+    }
+}
+
+const ghosts = [
+    new Ghost("binky", 348, 250),
+    new Ghost("pinky", 376, 400),
+    new Ghost("inky", 351, 300),
+    new Ghost("clyde", 379, 500)
+]
+
+//draw my ghosts into my grid
+ghosts.forEach(ghost => {
+    squares[ghost.currentIndex].classList.add(ghost.className);
+    squares[ghost.currentIndex].classList.add("ghost");
+})
+
+//move the ghosts
+ghosts.forEach(ghost => moveGhost(ghost));
+
+function moveGhost(ghost) {
+    console.log("moved ghost");
+    const directions = [-1, +1, -width, +width];
+    let direction = directions[Math.floor(Math.random() * directions.length)];
+
+    ghost.timerId = setInterval(() => {
+        //all our code
+        // if the next square does NOT contain a wall or does not contain a ghost
+        if (
+            !squares[ghost.currentIndex + direction].classList.contains("wall") &&
+            !squares[ghost.currentIndex + direction].classList.contains("ghost")
+        ) {
+        //remove any ghost class
+        squares[ghost.currentIndex].classList.remove(ghost.className);
+        squares[ghost.currentIndex].classList.remove("ghost");
+        //add direction to current index
+        ghost.currentIndex += direction;
+        //re-add ghost class
+        squares[ghost.currentIndex].classList.add(ghost.className);
+        squares[ghost.currentIndex].classList.add("ghost");
+        } else direction = directions[Math.floor(Math.random() * directions.length)];
+
+    }, ghost.speed);
+}
